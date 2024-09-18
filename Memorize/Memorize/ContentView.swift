e331154c74ac17873cf155c661803f49135afd2f
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-09-19 01:05:11 +0300

    Initial commit

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Ali on 18.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("hello vision pro!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ali on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
